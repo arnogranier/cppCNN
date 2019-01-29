7f4bc29e98591fa29d3ea2ad2e955264b15c00c8
Ndeye LO <lo@ensta.fr>
2019-01-29 12:25:22 +0100

    array3d

--- array3d.hpp
+++ array3d.hpp
@@ -3,5 +3,30 @@
 #include <iostream>
 using namespace std;
 
+class Array3d{
+private :
+    int height,width,depth;
+public :
+
+    Array3d();
+    int &  operator () (int i);
+    int  operator () const (int i);
+    Array3d & operator + (const Array3d & A);
+    Array3d & operator - (const Array3d & A);
+
+
+}
+
+
+Array3d & operator + (const Array3d & A,const Array3d & B);
+Array3d & operator - (const Array3d & A,const Array3d & B);
+Array3d operator + (const Array3d &  A , int x);
+Array3d operator - (const Array3d &  A , int x);
+Array3d operator /(constArray3d &  A , int x);
+Array3d operator *(const Array3d &  A , int x);
+Array3d operator *(const Array3d &  A , const Array3d &  B);
+Array3d operator *(int x, const Array3d &  A);
+bool operator ==(const Array3d & A, const Array3d & B);
+bool operator !=(const Array3d & A, const Array3d & B);
 #endif
 
