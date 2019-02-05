521835cd14b755d4f2948b46eca2470339d14e52
Arno Granier <arno.granier@u-psud.fr>
2019-02-05 01:41:30 +0100

    array3d.hpp typos

--- array3d.hpp
+++ array3d.hpp
@@ -3,5 +3,29 @@
 #include <iostream>
 using namespace std;
 
+class Array3d{
+private :
+    int height,width,depth;
+public :
+
+    Array3d();
+    int &  operator () (int i);
+    int  operator () (int i) const;
+    Array3d & operator + (const Array3d & A);
+    Array3d & operator - (const Array3d & A);
+
+
+};
+
+Array3d & operator + (const Array3d & A,const Array3d & B);
+Array3d & operator - (const Array3d & A,const Array3d & B);
+Array3d operator + (const Array3d &  A , int x);
+Array3d operator - (const Array3d &  A , int x);
+Array3d operator /(const Array3d &  A , int x);
+Array3d operator *(const Array3d &  A , int x);
+Array3d operator *(const Array3d &  A , const Array3d &  B);
+Array3d operator *(int x, const Array3d &  A);
+bool operator ==(const Array3d & A, const Array3d & B);
+bool operator !=(const Array3d & A, const Array3d & B);
 #endif
 
