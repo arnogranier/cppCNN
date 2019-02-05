eda0c534d44cf139bd53bef9ca31fb52d04a95b4
Arno Granier <arno.granier@u-psud.fr>
2019-02-05 12:24:51 +0100

    cnn database hpp

--- CNN.hpp
+++ CNN.hpp
@@ -1,6 +1,20 @@
 #ifndef CNN
 
 #include <iostream>
+#include <vector>
 using namespace std;
+
+class CNN
+{
+private:
+    vector<vector<double> > train_db_images;
+    vector<int8_t > train_db_labels;
+    vector<vector<double> > test_db_images;
+    vector<int8_t > test_db_labels;
+public:
+    void set_train_db(vector<vector<double> >, vector<int8_t >);
+    void set_test_db(vector<vector<double> >, vector<int8_t >);
+    void set_db(string, string, string, string);
+};
 
 #endif
